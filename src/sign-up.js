612de787ed294340966d640c0ebbfe2d8a83d437
Wadie Mendja <mendja2014@gmail.com>
2021-05-08 22:57:45 +0100

    commit

--- src/sign-up.js
+++ src/sign-up.js
@@ -6,17 +6,42 @@ firebase.initializeApp(firebaseConfig);
 
 const signupBtn = document.getElementById('signup_btn');
 
-signupBtn.addEventListener('click', ()=> {
+signupBtn.addEventListener('click', (event)=> {
+    // preventing the default action
+    event.preventDefault();
     const username = document.getElementById('username').value;
-    const frontPassword = document.getElementById('password').value;
+    const password = document.getElementById('password').value;
     const fullName = document.getElementById('fullName').value;
     const type = document.querySelector('.form-select').value;
     const newUserRef = firebase.database().ref('users/' + username);
-    const data = {
-        username: username,
-        password : password,
-        fullName: fullName,
-        type : type
-    }
-    newUserRef.set(data);
+    const usersRef = firebase.database().ref('users');
+    usersRef.once('value', (sanp)=>{
+        const users = sanp.val();        
+        const alert = document.getElementById("alert");
+        if (users[username] != null){
+            alert.innerHTML = `
+            <div style="color:red;">
+                This username already used by another account. Please try another username
+            </div><br>`;
+        } else {            
+            const data = {
+                username: username,
+                password : password,
+                fullName: fullName,
+                type : type
+            }
+            newUserRef.set(data);
+            resetForm();
+            alert.innerHTML =  `
+            <div style="color:green;">
+                You account has been created succefully. <a href="/">login here</a>
+            </div><br>`;
+        }
+        
+    });
 });
+
+function resetForm(){
+    const form = document.querySelector('form');
+    form.reset();
+}
