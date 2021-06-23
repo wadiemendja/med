a5e45050f40b78b8dfdf04bebdcb277509530e56
Wadie Mendja <mendja2014@gmail.com>
2021-06-24 00:14:02 +0100

    commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,18 @@
 export function getConfig() {
     return {
-        apiKey: "AIzaSyDs2XJBawaB8AM-K5ZMl0ZvXRlgniPPqyI",
+        apiKey: "AIzaSyD1bcLKcLRuQnTObOqUDp6zSH3yoTyLOhw",
+        authDomain: "dpdata-9affa.firebaseapp.com",
+        databaseURL: "https://dpdata-9affa-default-rtdb.firebaseio.com",
+        projectId: "dpdata-9affa",
+        storageBucket: "dpdata-9affa.appspot.com",
+        messagingSenderId: "624930881402",
+        appId: "1:624930881402:web:7c73b19aacc84fc84960c3"
+        /*apiKey: "AIzaSyDs2XJBawaB8AM-K5ZMl0ZvXRlgniPPqyI",
         authDomain: "tp-intg.firebaseapp.com",
         databaseURL: "https://tp-intg-default-rtdb.firebaseio.com",
         projectId: "tp-intg",
         storageBucket: "tp-intg.appspot.com",
         messagingSenderId: "758229529666",
-        appId: "1:758229529666:web:6aa10a8be4e8c5af3ea9db"
+        appId: "1:758229529666:web:6aa10a8be4e8c5af3ea9db"*/
     };
 }
